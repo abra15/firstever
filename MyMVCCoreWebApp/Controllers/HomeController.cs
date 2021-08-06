8e40eb8b41816e8005b039a4f2807166f452b818
rauttabh <rauttabh@W98Z8N62>
2021-08-06 12:40:03 +0530

    Add project files.

---
+++ MyMVCCoreWebApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MyMVCCoreWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMVCCoreWebApp.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration )
+    {
+      _logger = logger;
+      _configuration = configuration;
+    }
+
+    public IActionResult Index()
+    {
+      ViewBag.MySecret =  _configuration.GetValue<string>("MySecret");
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
